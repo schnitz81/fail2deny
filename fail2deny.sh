0ab025f89556611f9e4fa0c5dc2234d3ec311b7d
schnitz81 <k.quick81@gmail.com>
2017-06-04 04:09:50 +0200

    Added allow strings exceptions and occurence count fix.

--- fail2deny.sh
+++ fail2deny.sh
@@ -9,9 +9,10 @@ file4='/dev/null'
 file5='/dev/null'
 
 
-denyfile='/tmp/hosts.deny'
+denyfile='/etc/hosts.deny'
 
-failstrings='-e fail -e Invalid'  # ******************** Search words (case insensitive). The strings in log that are considered fail attempts. 
+failstrings='-i -e fail -e Invalid'  # ******************** Search words (case insensitive). The strings in log that are considered fail attempts. 
+allowstrings='-v -i -e check'  # ************************** Exceptions strings that will override the search words.
 
 echo -n "$(date +"%Y%m%d %H:%M:%S")  "  # Put timestamp
 echo "Starting to monitoring files..."
@@ -31,18 +32,18 @@ do
 		# Find last IP with failed login.
 		echo
 		echo "Checking $logfile..." 
-		ipToCheck=`cat $logfile | grep -i $failstrings | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+' | tail -n 1`
+		ipToCheck=`cat $logfile | grep $failstrings | grep $allowstrings | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+' | tail -n 1`
 	
 		if [[ ! -z  $ipToCheck ]]; then  # if not empty string
 			echo
 			echo "IP number $ipToCheck has made one or more failed access attempts."
 			echo
 			echo -n "No of failed access attempts: "
-			echo `grep -e $ipToCheck $logfile | grep $failstrings | wc -l`   # Print no of occurances
+			echo `grep $ipToCheck $logfile | grep $failstrings | grep $allowstrings | wc -l`   # Print no of occurances
 			echo
-			if [ `grep -e $ipToCheck $logfile | grep $failstrings | wc -l` -gt 4 ]; then  # Check if no of occurances > 4
-				earlyTime=`cat $logfile | grep -e $ipToCheck $failstrings | tail -n 5 | grep -o '.\{0,2\}\:.\{0,2\}' | head -n 1`  # Get fifth last time
-				lateTime=`cat $logfile | grep -e $ipToCheck $failstrings | tail -n 1 | grep -o '.\{0,2\}\:.\{0,2\}' | head -n 1`  # Get last time
+			if [ `grep $ipToCheck $logfile | grep $failstrings | grep $allowstrings | wc -l` -gt 4 ]; then  # Check if no of occurances > 4
+				earlyTime=`cat $logfile | grep -e $ipToCheck $failstrings | grep $allowstrings | tail -n 5 | grep -o '.\{0,2\}\:.\{0,2\}' | head -n 1`  # Get fifth last time
+				lateTime=`cat $logfile | grep -e $ipToCheck $failstrings | grep $allowstrings | tail -n 1 | grep -o '.\{0,2\}\:.\{0,2\}' | head -n 1`  # Get last time
 			else
 				echo "Less than five occurances. Will not ban."; echo "-------------------"; continue  # Break loop iteration.
 			fi
